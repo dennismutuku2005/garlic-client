be6854c5650e7a50417c0cdc2316d176b3cf2ee3
DENNIS MUUO <dennismuuo.dev@gmail.com>
2026-08-28 22:31:29 +0300

    Simplify TrapError and FatalError messages to return only raw router string

--- internal/protocol/errors.go
+++ internal/protocol/errors.go
@@ -10,10 +10,7 @@ type TrapError struct {
 }
 
 func (e *TrapError) Error() string {
-	if e.Category != "" {
-		return fmt.Sprintf("RouterOS trap [category %s]: %s", e.Category, e.Message)
-	}
-	return fmt.Sprintf("RouterOS trap: %s", e.Message)
+	return e.Message
 }
 
 // FatalError is returned when the router replies with a !fatal sentence.
@@ -23,7 +20,7 @@ type FatalError struct {
 }
 
 func (e *FatalError) Error() string {
-	return fmt.Sprintf("RouterOS fatal: %s", e.Message)
+	return e.Message
 }
 
 // ErrNotConnected is returned when an operation is attempted on a disconnected client.
